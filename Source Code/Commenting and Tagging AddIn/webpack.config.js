fce55b94a0b104b688286ceda5689b41fe075b3b
dhyeys249 <dhyey@desireinfoweb.com>
2026-06-01 16:22:47 +0530

    commit

--- Source Code/Commenting and Tagging AddIn/webpack.config.js
+++ Source Code/Commenting and Tagging AddIn/webpack.config.js
@@ -5,7 +5,7 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlProd = "https://desireadmin.github.io/MarcoCSG-Commenting-Word-Add-In/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
