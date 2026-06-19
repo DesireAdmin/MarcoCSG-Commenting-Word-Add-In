a0e5c1b77adc2e7e54da99855c76ecd12462f93c
dhyeys249 <dhyey@desireinfoweb.com>
2026-06-19 13:46:53 +0530

    commit

--- Source Code/MentionNotifier/webpack.config.js
+++ Source Code/MentionNotifier/webpack.config.js
@@ -5,7 +5,7 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://r365-training.regdocs365.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlProd = "https://subs-tst.regdocs365.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
